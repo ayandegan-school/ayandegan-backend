fa11017cb9f3020c86691c32da8a1790797f197f
mkazemiraz <kazemiraz72@gmail.com>
2020-09-01 01:54:30 +0430

    authentication with jwt

---
+++ README.md
@@ -0,0 +1,27 @@
+# Ayandegan High school Project
+Ayandegan Non-Profit High School Website Development Project
+
+## summary
+* [Getting Started](#getting-started)
+* [Authors](#authors)
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+1. [Install node & npm](https://nodejs.org/en/download/)
+
+### Installing
+A step by step series of examples that tell you how to get a development env running
+1. Open your terminal & navigate to project directory
+2. Install node_modules
+
+    `npm install --save-dev  `
+3. Run the node command in your project directory
+
+    `npm start`
+
+## Authors
+
+* [Mkazemiraz](https://github.com/mkazemiraz)
+* [Mona-Nasrollahi](https://github.com/mona-n67)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/task');
+
+// Routes
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
